c797aa749235836cb35de2e40343cc650092e720
abubakarmansur714-rgb <abubakarmansur714@gmail.com>
2026-08-19 16:06:00 +0100

    Add package.json for Clinic Appointment System

---
+++ Package.js
@@ -0,0 +1,15 @@
+# Clinic-Appointment-System
+{
+  "name": "clinic-appointment-system",
+  "version": "1.0.0",
+  "description": "3MTT Cohort 3 Kebbi - Clinic Appointment System",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "node server.js"
+  },
+  "dependencies": {
+    "better-sqlite3": "^11.10.0",
+    "express": "^5.1.0"
+  }
+}
